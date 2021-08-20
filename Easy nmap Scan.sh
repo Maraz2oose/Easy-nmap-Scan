9007d7e4984938f1e0445436f558828f9f763e9d
Maraz2oose <89168962+Maraz2oose@users.noreply.github.com>
2021-08-19 21:57:41 -0700

    Update Easy nmap Scan.sh

--- Easy nmap Scan.sh
+++ Easy nmap Scan.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 echo Hello welcome to Marazmoose\'s Easy nmap Scanner. If you havent already please do sudo install nmap in terminal.
 
 
